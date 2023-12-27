0ded4546feba0005333c52945c644ed006a99a4e
edwinfernaldy <edwinfernaldy@gmail.com>
2023-12-27 10:38:36 +0700

    remove POST method on login

--- src/app/(routes)/api/login/route.ts
+++ src/app/(routes)/api/login/route.ts
@@ -3,7 +3,3 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest, res: NextResponse) {
   return NextResponse.json({ success: true });
 }
-
-export async function POST(req: NextRequest, res: NextResponse) {
-  return {};
-}
