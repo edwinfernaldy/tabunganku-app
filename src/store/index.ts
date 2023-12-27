e6f48454b797a950dd2215c846e35c6c86b266c1
edwinfernaldy <edwinfernaldy@gmail.com>
2023-12-27 22:04:46 +0700

    add user session store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,30 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface LandingState {
   type: "login" | "register";
   setType: (params: "login" | "register") => void;
 }
 
+interface SessionState {
+  userId: string;
+  setUserId: (params: string) => void;
+}
+
 export const useLandingStore = create<LandingState>()((set) => ({
   type: "login",
   setType: (params: "login" | "register") => set(() => ({ type: params }))
 }));
+
+export const useSessionStore = create<SessionState>()(
+  persist(
+    (set) => ({
+      userId: "",
+      setUserId: (params: string) => set(() => ({ userId: params }))
+    }),
+    {
+      name: "user-storage",
+      storage: createJSONStorage(() => sessionStorage)
+    }
+  )
+);
