f6df7676fea7897f7241e6f013128d9c9c727072
edwinfernaldy <edwinfernaldy@gmail.com>
2023-12-27 21:41:43 +0700

    add to transaction

--- src/app/(routes)/dashboard/transaction/page.tsx
+++ src/app/(routes)/dashboard/transaction/page.tsx
@@ -2,6 +2,8 @@
 
 import Button from "@/components/Button";
 import Card from "@/components/Card";
+import Input from "@/components/Input";
+import Modal from "@/components/Modal";
 import Table from "@/components/Table";
 import { Transaction } from "@prisma/client";
 import React, { useEffect, useState } from "react";
@@ -9,6 +11,7 @@ import { IoMdAddCircle } from "react-icons/io";
 
 const Transaction: React.FC = () => {
   const [data, setData] = useState<Transaction[]>();
+  const [open, setOpen] = useState<boolean>(false);
 
   const getTransactionData = async () => {
     await fetch("/api/transaction", {
@@ -32,10 +35,62 @@ const Transaction: React.FC = () => {
       </h1>
 
       <div className='relative flex justify-end'>
-        <Button className='w-fit flex items-center gap-2'>
+        <Button
+          onClick={() => setOpen(true)}
+          className='w-fit flex items-center gap-2'
+        >
           <IoMdAddCircle className='text-2xl' />
           New Transaction
         </Button>
+
+        {open && (
+          <Modal onClose={() => setOpen(false)} header={"Add Transaction"}>
+            <div className='space-y-4 mt-5'>
+              <div className='flex gap-2 items-center w-full'>
+                <div className='flex flex-col gap-2 basis-1/2'>
+                  <label>Type</label>
+
+                  <select className='w-full border border-gray-400 p-3 rounded-md appearance-none'>
+                    <option value={"INCOME"}>Income</option>
+                    <option value={"EXPENSE"}>Expense</option>
+                  </select>
+                </div>
+
+                <div className='flex flex-col gap-2 basis-1/2'>
+                  <label>Transaction Date</label>
+                  <Input
+                    className='border border-gray-400'
+                    type={"date"}
+                    onChange={() => {}}
+                    value={""}
+                  />
+                </div>
+              </div>
+
+              <div className='flex flex-col gap-2'>
+                <label>Amount</label>
+                <Input
+                  className='border border-gray-400'
+                  type={"text"}
+                  onChange={() => {}}
+                  value={""}
+                />
+              </div>
+
+              <div className='flex flex-col gap-2'>
+                <label>Description</label>
+                <Input
+                  className='border border-gray-400'
+                  type={"text"}
+                  onChange={() => {}}
+                  value={""}
+                />
+              </div>
+
+              <Button>Submit</Button>
+            </div>
+          </Modal>
+        )}
       </div>
 
       <Card>
