84f99b31016a79eb9541c076eea9d5bf59f8a026
edwinfernaldy <edwinfernaldy@gmail.com>
2023-12-28 20:08:20 +0700

    add todays income and expense api

---
+++ src/app/(routes)/api/today-expense/route.ts
@@ -0,0 +1,39 @@
+import { BadRequestException } from "@/server/http.errors";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../../lib/prisma";
+
+export async function POST(req: NextRequest) {
+  try {
+    const data = (await req.json()) as { user_id: string };
+
+    if (!data.user_id) {
+      throw new BadRequestException("Missing User Id");
+    }
+
+    const income = await prisma.transaction.findMany({
+      where: {
+        type: "EXPENSE",
+        date: new Date().toISOString(),
+        user_id: data.user_id
+      }
+    });
+
+    return NextResponse.json(income);
+  } catch (e) {
+    const error = e as Error;
+
+    if (error instanceof BadRequestException) {
+      return NextResponse.json({
+        error: {
+          message: error.message
+        }
+      });
+    } else {
+      return NextResponse.json({
+        error: {
+          message: "Internal server error"
+        }
+      });
+    }
+  }
+}
