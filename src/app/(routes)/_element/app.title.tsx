386c61a6fa95759a1743a1dfd3add424f452e9d8
edwinfernaldy <edwinfernaldy@gmail.com>
2023-12-29 10:04:24 +0700

    add lottie and app title

---
+++ src/app/(routes)/_element/app.title.tsx
@@ -0,0 +1,7 @@
+export default function AppTitle() {
+  return (
+    <h1 className='text-blue-600 font-extrabold text-4xl tracking-widest lg:text-6xl'>
+      TABUNGAN <span className='text-blue-900'>KU</span>
+    </h1>
+  );
+}
--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -3,15 +3,21 @@
 import { useLandingStore } from "@/store";
 import LoginSection from "./_element/login";
 import RegisterSection from "./_element/register";
+import animation from "../../../public/assets/lottie-animation.json";
+import Lottie from "lottie-react";
+import AppTitle from "./_element/app.title";
 
 export default function Home() {
   const { type } = useLandingStore();
 
   return (
     <main className='flex h-screen items-center overflow-hidden justify-between'>
-      <div className='lg:basis-1/2 hidden lg:block'></div>
+      <div className='lg:basis-1/2 hidden lg:flex justify-center items-center'>
+        <Lottie animationData={animation} loop />
+      </div>
 
       <div className='bg-blue-400 shadow-lg w-full border-blue-800 h-full flex flex-col gap-8 justify-center lg:basis-1/2 p-8 lg:p-24'>
+        <AppTitle />
         {type === "login" ? <LoginSection /> : <RegisterSection />}
       </div>
     </main>
